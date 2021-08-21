a42d98d57b5ff527e27a8fedb6e304676893f1f7
MeteoroidShot <ejada.inv@gmail.com>
2021-08-21 07:55:05 +0300

    remove useless log message

--- rules.js
+++ rules.js
@@ -227,9 +227,6 @@ module.exports = {
         if(firstNumber == secondNumber)
             return [0, `${firstNumber} = ${secondNumber}`];
 
-        if(firstNumber == '7' || secondNumber == '7')
-            console.log('APPLY', firstNumber, secondNumber, rulesObtained);
-
         if(getValueRulesOnly(firstNumber,secondNumber).length > 0)
             return [0, `${firstNumber} has the value of ${secondNumber}`]; // they have the same value so equate
 
