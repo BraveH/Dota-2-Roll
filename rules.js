cf454b8ea5f67a82ea739ec29734e496dc363a88
MeteoroidShot <ejada.inv@gmail.com>
2021-09-21 02:58:08 +0200

    trial 1

--- rules.js
+++ rules.js
@@ -156,10 +156,10 @@ const addRule = (id, type, numberOne, numberTwo, description) => {
 }
 
 const deleteRules = (ruleIds) => {
-    let ruleIdsString = ruleIds//.map(t => `'${t}'`).join(',');
+    let ruleIdsString = ruleIds.map(t => `'${t}'`).join(',');
     console.log("Deleting rules: ", ruleIds)
     console.log("Deleting rules 2: ", ruleIdsString)
-    return db.query(DELETE_SQL, [ruleIdsString]).then(r => {
+    return db.query(DELETE_SQL, ruleIds).then(r => {
         console.log("DELETED. result", r);
         return Promise.resolve();
     }).catch(e => {
